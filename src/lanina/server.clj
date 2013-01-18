f8e9aaaf33173985359f1cc1e057addc0316a9c3
cesarbp <cesarbol@gmail.com>
2013-01-18 12:10:54 -0600

    Initialize mongo at the start

--- src/lanina/server.clj
+++ src/lanina/server.clj
@@ -1,11 +1,13 @@
 (ns lanina.server
   (:gen-class)
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+            [clj-commons-exec :as exec]))
 
 (server/load-views-ns 'lanina.views)
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
+    @(exec/sh ["mongod"])
     (server/start port {:mode mode
                         :ns 'lanina})))
